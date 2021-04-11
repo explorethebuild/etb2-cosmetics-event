ce1cd2bc76b9fc8075031a4d52806d9f94d1e09d
Justus <justus@justus-alvermann.de>
2021-04-11 17:57:08 +0200

    Added several blocks - obsidian - lapis ore - ancient debris - crying obsidian - prismarine

--- src/main/kotlin/dev/just/etb2cosmeticsevent/listeners/BreakBlockListener.kt
+++ src/main/kotlin/dev/just/etb2cosmeticsevent/listeners/BreakBlockListener.kt
@@ -2,6 +2,7 @@ package dev.just.etb2cosmeticsevent.listeners
 
 import dev.just.etb2cosmeticsevent.Main
 import dev.just.etb2cosmeticsevent.utils.addCoins
+import dev.just.etb2cosmeticsevent.utils.removeCoins
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -15,5 +16,10 @@ class BreakBlockListener : Listener {
         else if (event.block.type.equals(Material.EMERALD_ORE)) addCoins(event.player, 60)
         else if (event.block.type.equals(Material.IRON_ORE)) addCoins(event.player, 10)
         else if (event.block.type.equals(Material.COAL_ORE)) addCoins(event.player, 3)
+        else if (event.block.type.equals(Material.OBSIDIAN)) addCoins(event.player, 5)
+        else if (event.block.type.equals(Material.LAPIS_ORE)) addCoins(event.player, 6)
+        else if (event.block.type.equals(Material.ANCIENT_DEBRIS)) addCoins(event.player, 70)
+        else if (event.block.type.equals(Material.CRYING_OBSIDIAN)) removeCoins(event.player, 3)
+        else if (event.block.type.equals(Material.PRISMARINE)) removeCoins(event.player, 2)
     }
 }
