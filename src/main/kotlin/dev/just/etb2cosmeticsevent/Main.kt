2208e3feb030a39039765490f408e504b93afc6e
Justus <justus@justus-alvermann.de>
2021-04-11 17:35:21 +0200

    added main functionality register commands, listeners init coreprotect init points give function give out prefix

---
+++ src/main/kotlin/dev/just/etb2cosmeticsevent/Main.kt
@@ -0,0 +1,49 @@
+package dev.just.etb2cosmeticsevent
+import dev.just.etb2cosmeticsevent.commands.EventInfoCommand
+import dev.just.etb2cosmeticsevent.listeners.BreakBlockListener
+import dev.just.etb2cosmeticsevent.listeners.JoinListener
+import dev.just.etb2cosmeticsevent.utils.CoreProtectHelper
+import dev.just.etb2cosmeticsevent.utils.giveOnlinePlayersCoins
+import net.coreprotect.CoreProtectAPI
+import net.md_5.bungee.api.ChatColor
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandExecutor
+import org.bukkit.event.Listener
+import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.PluginManager
+import org.bukkit.plugin.java.JavaPlugin
+
+public class Main : JavaPlugin(), Listener, CommandExecutor {
+    override fun onEnable() {
+        instance = this
+        Bukkit.getLogger().info("Plugin loaded successfully");
+        registerListeners()
+        registerCommands()
+        giveOnlinePlayersCoins()
+        coreProtectHelper()!!.testApi()
+        dev.just.etb2cosmeticsevent.utils.init()
+    }
+
+    private fun registerListeners(): Unit {
+        val pluginManager: PluginManager = Bukkit.getPluginManager()
+        pluginManager.registerEvents(JoinListener(), this)
+        pluginManager.registerEvents(BreakBlockListener(), this)
+    }
+    private fun registerCommands(): Unit {
+        getCommand("eventinfo")?.setExecutor(EventInfoCommand())
+    }
+    companion object {
+        private fun coreProtectApi(): CoreProtectAPI? {
+            return CoreProtectHelper.initApi(Bukkit.getServer())
+        }
+        private var cPHelper: CoreProtectHelper? = null
+        fun coreProtectHelper(): CoreProtectHelper? {
+            if (cPHelper == null) {
+                cPHelper = CoreProtectHelper(coreProtectApi()!!)
+            }
+            return cPHelper
+        }
+        val prefix: String = "${ChatColor.GRAY}[${ChatColor.BLUE}Event${ChatColor.GRAY}]${ChatColor.DARK_GRAY} "
+        var instance: Plugin? = null
+    }
+}
