573cc8d4c0709d48dfca81598bf4de10216a7a16
Justus <justus@justus-alvermann.de>
2021-04-12 14:46:46 +0200

    added mobs from https://github.com/explorethebuild/etb2-cosmetics-event/wiki/Points-trough-mob-kills

--- src/main/kotlin/dev/just/etb2cosmeticsevent/listeners/MobKillListener.kt
+++ src/main/kotlin/dev/just/etb2cosmeticsevent/listeners/MobKillListener.kt
@@ -1,5 +1,7 @@
 package dev.just.etb2cosmeticsevent.listeners
 
+import dev.just.etb2cosmeticsevent.utils.addCoins
+import dev.just.etb2cosmeticsevent.utils.removeCoins
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -12,5 +14,27 @@ class MobKillListener: Listener {
         if (!event.entity.isDead) return
         if (event.damager !is Player) return
         val entityType: EntityType = event.entity.type
+        val player: Player = event.damager as Player
+        when (entityType) {
+            EntityType.ENDER_DRAGON -> addCoins(player, 200)
+            EntityType.WITHER -> addCoins(player, 150)
+            EntityType.ELDER_GUARDIAN -> addCoins(player, 75)
+            EntityType.PIGLIN_BRUTE -> addCoins(player, 75)
+            EntityType.EVOKER -> addCoins(player, 60)
+            EntityType.SHULKER -> addCoins(player, 30)
+            EntityType.WITCH -> addCoins(player, 10)
+            EntityType.WITHER_SKELETON -> addCoins(player, 10)
+            EntityType.BLAZE -> addCoins(player, 8)
+            EntityType.GHAST -> addCoins(player, 8)
+            EntityType.MAGMA_CUBE -> addCoins(player, 8)
+            EntityType.SLIME -> addCoins(player, 6)
+            EntityType.IRON_GOLEM -> addCoins(player,5)
+            EntityType.CREEPER -> addCoins(player,4)
+            EntityType.PHANTOM -> addCoins(player, 3)
+            EntityType.SKELETON -> addCoins(player ,2)
+            EntityType.BOAT -> removeCoins(player, 2)
+            EntityType.VILLAGER -> removeCoins(player, 100)
+            else -> return
+        }
     }
 }
