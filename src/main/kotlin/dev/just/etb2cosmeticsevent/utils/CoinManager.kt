60dc25825b89f892293eb3bb7f5b256f4d100fee
Justus <justus@justus-alvermann.de>
2021-04-12 15:31:14 +0200

    Refactored, added custom tablist Added getcoins function to get the amount of coins of the specified player Enable negative coins Change logical order so the actionbar is a confirmation of the transfer Removed public keywords as they are not needed

--- src/main/kotlin/dev/just/etb2cosmeticsevent/utils/CoinManager.kt
+++ src/main/kotlin/dev/just/etb2cosmeticsevent/utils/CoinManager.kt
@@ -7,8 +7,7 @@ import net.md_5.bungee.api.chat.BaseComponent
 import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.entity.Player
 
-public fun addCoins(player: Player, coins: Int): Unit {
-    sendActionbar(player, TextComponent("${ChatColor.GOLD}+${coins.toString()} ${if (coins == 1) "Coin" else "Coins"}"))
+fun addCoins(player: Player, coins: Int): Unit {
     val playerModel: PlayerModel? = jsonDBTemplate.findById(player.uniqueId.toString(), PlayerModel::class.java)
     if (playerModel == null) {
         val newPlayerModel: PlayerModel = PlayerModel()
@@ -19,18 +18,44 @@ public fun addCoins(player: Player, coins: Int): Unit {
         playerModel.setCoins((playerModel.getCoins()!!.toInt() + coins).toString())
         jsonDBTemplate.save<PlayerModel>(playerModel, PlayerModel::class.java)
     }
+    sendActionbar(player, TextComponent("${ChatColor.GOLD}+${coins.toString()} ${if (coins == 1) "Coin" else "Coins"}"))
+    setTab(player)
 }
 
-public fun removeCoins(player: Player, coins: Int): Unit {
-    sendActionbar(player, TextComponent("${ChatColor.RED}-${coins.toString()} Coins"))
+fun removeCoins(player: Player, coins: Int): Unit {
     val playerModel: PlayerModel? = jsonDBTemplate.findById(player.uniqueId.toString(), PlayerModel::class.java)
-    if (playerModel == null) {
-    } else {
+    if (playerModel != null) {
         playerModel.setCoins((playerModel.getCoins()!!.toInt() - coins).toString())
         jsonDBTemplate.save<PlayerModel>(playerModel, PlayerModel::class.java)
+    } else {
+        val newPlayerModel: PlayerModel = PlayerModel()
+        newPlayerModel.setUuid(player.uniqueId.toString())
+        newPlayerModel.setCoins(coins = (coins * -1).toString())
+        jsonDBTemplate.insert<PlayerModel>(newPlayerModel)
+    }
+    sendActionbar(player, TextComponent("${ChatColor.RED}-${coins.toString()} Coins"))
+    setTab(player)
+}
+
+fun getCoins(player: Player): Int {
+    val playerModel: PlayerModel = jsonDBTemplate.findById(player.uniqueId.toString(), PlayerModel::class.java)
+        ?: return 0
+    return if (playerModel.getCoins() == null) {
+        0
+    } else {
+        playerModel.getCoins()!!.toInt()
     }
 }
 
 private fun sendActionbar(player: Player, message: BaseComponent): Unit {
     player.spigot().sendMessage(ChatMessageType.ACTION_BAR, message)
+}
+
+fun setTab(player: Player) {
+    val header: String = "${ChatColor.GRAY}Currently, a ${ChatColor.BLUE}COSMETICS EVENT ${ChatColor.GRAY}is running!" +
+            "\nCollect ${ChatColor.BLUE}items ${ChatColor.GRAY}and kill ${ChatColor.BLUE}mobs ${ChatColor.GRAY}to " +
+            "collect points!"
+    val footer: String = "${ChatColor.GRAY}You currently have ${ChatColor.BLUE}${getCoins(player)} " +
+            "coins${ChatColor.GRAY}!"
+    player.setPlayerListHeaderFooter(header, footer)
 }
