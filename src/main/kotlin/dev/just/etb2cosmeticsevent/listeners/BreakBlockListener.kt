c0fc4d2da719ee4c8996639503a49f305aa5257f
Justus <justus@justus-alvermann.de>
2021-04-12 14:51:17 +0200

    added blocks from https://github.com/explorethebuild/etb2-cosmetics-event/wiki/Points-trough-mining

--- src/main/kotlin/dev/just/etb2cosmeticsevent/listeners/BreakBlockListener.kt
+++ src/main/kotlin/dev/just/etb2cosmeticsevent/listeners/BreakBlockListener.kt
@@ -10,16 +10,23 @@ import org.bukkit.event.block.BlockBreakEvent
 
 class BreakBlockListener : Listener {
     @EventHandler
-    public fun onBreak(event: BlockBreakEvent) {
+    fun onBreak(event: BlockBreakEvent) {
         if (Main.coreProtectHelper()!!.isBlockPlayermade(event.block)) return
-        if (event.block.type.equals(Material.DIAMOND_ORE)) addCoins(event.player,40)
-        else if (event.block.type.equals(Material.EMERALD_ORE)) addCoins(event.player, 60)
-        else if (event.block.type.equals(Material.IRON_ORE)) addCoins(event.player, 10)
-        else if (event.block.type.equals(Material.COAL_ORE)) addCoins(event.player, 3)
-        else if (event.block.type.equals(Material.OBSIDIAN)) addCoins(event.player, 5)
-        else if (event.block.type.equals(Material.LAPIS_ORE)) addCoins(event.player, 6)
-        else if (event.block.type.equals(Material.ANCIENT_DEBRIS)) addCoins(event.player, 70)
-        else if (event.block.type.equals(Material.CRYING_OBSIDIAN)) removeCoins(event.player, 3)
-        else if (event.block.type.equals(Material.PRISMARINE)) removeCoins(event.player, 2)
+        when (event.block.type) {
+            Material.EMERALD_ORE -> addCoins(event.player, 65)
+            Material.ANCIENT_DEBRIS -> addCoins(event.player, 60)
+            Material.DIAMOND_ORE -> addCoins(event.player, 40)
+            Material.LAPIS_ORE -> addCoins(event.player, 30)
+            Material.GOLD_ORE -> addCoins(event.player, 20)
+            Material.REDSTONE_ORE -> addCoins(event.player, 15)
+            Material.IRON_ORE -> addCoins(event.player, 6)
+            Material.NETHER_GOLD_ORE -> addCoins(event.player, 5)
+            Material.OBSIDIAN -> addCoins(event.player, 5)
+            Material.COAL_ORE -> addCoins(event.player, 1)
+            Material.NETHER_QUARTZ_ORE -> addCoins(event.player, 1)
+            Material.PRISMARINE -> removeCoins(event.player, 2)
+            Material.CRYING_OBSIDIAN -> removeCoins(event.player, 3)
+            else -> return
+        }
     }
 }
