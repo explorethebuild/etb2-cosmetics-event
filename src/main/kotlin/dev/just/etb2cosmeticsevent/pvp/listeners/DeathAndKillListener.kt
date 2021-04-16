f9be4210b826df1a571ab44ab99f64680e11c153
Justus <justus@justus-alvermann.de>
2021-04-16 16:59:16 +0200

    Added keepInventory if PVP kill

--- src/main/kotlin/dev/just/etb2cosmeticsevent/pvp/listeners/DeathAndKillListener.kt
+++ src/main/kotlin/dev/just/etb2cosmeticsevent/pvp/listeners/DeathAndKillListener.kt
@@ -1,5 +1,6 @@
 package dev.just.etb2cosmeticsevent.pvp.listeners
 
+import dev.just.etb2cosmeticsevent.Main
 import dev.just.etb2cosmeticsevent.pvp.addDeath
 import dev.just.etb2cosmeticsevent.pvp.addKill
 import dev.just.etb2cosmeticsevent.pvp.playerKillsTroughPvP
@@ -19,6 +20,11 @@ class DeathAndKillListener: Listener {
             addKill(event.entity.killer!!)
             removeCoins(event.entity, 40)
             addDeath(event.entity)
+            event.keepInventory = true
+            event.keepLevel = true
+            event.entity.sendMessage("${Main.prefix}Du bist gestorben, hast deine Items jedoch nicht verloren. ")
+            event.entity.sendMessage("${Main.prefix}Achtung: Diese Einschränkung gilt nur während des PvP-Events" +
+                    " und nur durch Kills durch Spieler. ")
         }
     }
 }
